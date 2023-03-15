ec2b25d7a714224c99bff448a68175db7dc5cc7d
RalucaVA <127589598+RalucaVA@users.noreply.github.com>
2023-03-15 12:02:34 +0200

    Add files via upload

---
+++ bubblesort.py
@@ -0,0 +1,26 @@
+import time
+st = time.time()
+
+def bubblesort(data):
+    n = len(data)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if data[j] > data[j+1]:
+                data[j], data[j+1] = data[j+1], data[j]
+  
+  
+import random;
+if __name__ == "__main__":
+  data = []
+  for i in range(9999):
+    x=random.randrange(0, 30033)
+    data.append(x)
+  
+  bubblesort(data)
+
+  for i in range(len(data)):
+      print("%d" % data[i], end=" ")
+
+et = time.time()
+elapsed_time = et - st
+print('Execution time:', elapsed_time, 'seconds')
